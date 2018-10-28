9d022c479348c49e4347c4e6f9245da01313746f
smith-chris <kowalski.k@outlook.com>
2018-10-28 10:32:19 +0000

    Classic arkanoid example.

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,4 +1,5 @@
-import { stage } from 'app/app'
+import { stage, ticker, viewport } from 'app/app'
+import { Point } from 'utils/point'
 
 const makeCircle = (circleSize: number) => {
   const result = new PIXI.Graphics()
@@ -12,3 +13,22 @@ const makeCircle = (circleSize: number) => {
 const circle = makeCircle(20)
 
 stage.addChild(circle)
+
+const circleSpeed = new Point(2, 2)
+
+ticker.add(delta => {
+  circle.position.x += circleSpeed.x * delta
+  circle.position.y += circleSpeed.y * delta
+  if (circle.getBounds().right >= viewport.width) {
+    circleSpeed.x = -Math.abs(circleSpeed.x)
+  }
+  if (circle.getBounds().bottom >= viewport.height) {
+    circleSpeed.y = -Math.abs(circleSpeed.y)
+  }
+  if (circle.getBounds().left <= 0) {
+    circleSpeed.x = Math.abs(circleSpeed.x)
+  }
+  if (circle.getBounds().top <= 0) {
+    circleSpeed.y = Math.abs(circleSpeed.y)
+  }
+})
