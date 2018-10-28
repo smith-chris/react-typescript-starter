386920b1470a65a601a42a46824ba474699e67fe
smith-chris <kowalski.k@outlook.com>
2018-10-28 10:25:40 +0000

    Playing around with raw pixi.

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { render } from '@inlet/react-pixi'
-import { Provider } from 'react-redux'
-
 import { stage } from 'app/app'
-import { store } from 'store/store'
-import { Game } from 'components/Game'
 
-render(
-  <Provider store={store}>
-    <Game />
-  </Provider>,
-  stage,
-)
+const makeCircle = (circleSize: number) => {
+  const result = new PIXI.Graphics()
+  result.lineStyle(0)
+  result.beginFill(0xffffff, 1)
+  result.drawCircle(circleSize, circleSize, circleSize)
+  result.endFill()
+  return result
+}
+
+const circle = makeCircle(20)
+
+stage.addChild(circle)
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Rect } from './Rect'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { gameActions } from 'store/game'
 
-export const Game = () => {
+const withStore = connect(
+  (state: StoreState) => state.game,
+  (dispatch: Dispatch) => {
+    return bindActionCreators({ ...gameActions }, dispatch)
+  },
+)
+
+export const Game = withStore(({ increment }) => {
   return (
     <>
       <Rect x={0} y={0} width={100} height={100} />
     </>
   )
-}
+})
