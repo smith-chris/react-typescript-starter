5e090f53e47e6edf73d76d695c27f6602bb95713
smith-chris <kowalski.k@outlook.com>
2019-02-25 11:23:22 +0000

    Basic bunny example.

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,1 +1,15 @@
-console.info('Wut')
+import { Sprite } from 'pixi.js'
+
+import { stage, ticker, viewport } from 'app/app'
+import bunnyImage from 'assets/bunny.png'
+
+const bunny = Sprite.fromImage(bunnyImage.src)
+bunny.anchor.set(0.5)
+bunny.x = viewport.width / 2
+bunny.y = viewport.height / 2
+
+stage.addChild(bunny)
+
+ticker.add(delta => {
+  bunny.rotation += 0.1 * delta
+})
